92caa5ed967c7d29dd8e329d1bed07bf7f837c90
unknown <lance.cantrell84@gmail.com>
2021-06-28 23:23:38 -0400

    Added more to the script.

--- add_device.py
+++ add_device.py
@@ -2,3 +2,12 @@ import requests
 import json
 from get_token import get_token
 
+
+# Define local varibles
+url = 'https://sandboxdnac.cisco.com'
+token = get_token()
+headers = {'X-Auth-Token': token, 'Content-Type': 'application/json', 'Accept-Type': 'application/json'}
+
+# Define the device we want to add as a dictionary
+new_device ={}
+

2514825848ea1a25b1baa3934629263af9f8d879
unknown <lance.cantrell84@gmail.com>
2021-06-28 22:09:11 -0400

    Rewrote the script so that it functions and can be called directly or imported as a module.

--- get_token.py
+++ get_token.py
@@ -1,12 +1,20 @@
 import requests
+import json
 
 # Define local varibles
 url = 'https://sandboxdnac.cisco.com/dna/system/api/v1/auth/token'
-headers = {'Auhorizatoin': 'basic ZGV2bmV0dXNlcjpDaXNjbzEyMyE='}
+auth = ("devnetuser", "Cisco123!")
+headers = {'Content-Type': 'application/json', 'Accept-Type': 'application/json'}
 
 def get_token():
     """Method used to get and return an X-Auth-Token for DNAC"""
-    response = requests.post(url=url, headers=headers)
-    token = json.dumps(response.json([Token]))
-    print(token)
+    response = requests.post(url=url, auth=auth, headers=headers)
+    token = response.json()['Token']
     return token
+
+def main():
+    token = get_token()
+    print(token)
+
+if __name__ == "__main__":
+    main()
